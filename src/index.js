c6009b3b78dd5f38ed532dfd40f9fa1dc5e3605d
Eduard <timiseduard@yahoo.com>
2022-11-26 18:34:12 +0100

    created card fragment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import Card from './Card'
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>
 );
 
