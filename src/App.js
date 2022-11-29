612e9ea4a6add6360d15ba7910c3cf214b18c267
Eduard <timiseduard@yahoo.com>
2022-11-29 13:58:18 +0100

    added a scroll element for robots

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
+import Scroll from './Scroll';
 // import { robots } from './robots.js';
 import SearchBox from './SearchBox'
 import './App.css';
@@ -29,13 +30,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-        <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-        </div>
-    )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+             return (
+                <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+                 </div>
+                )
+            }
     }
     
 }
