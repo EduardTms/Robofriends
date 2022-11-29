76a10292cb5a0104f1b3a6e60d1a850b527a4a6e
Eduard <timiseduard@yahoo.com>
2022-11-29 15:09:33 +0100

    implement HashRouter for github deployment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter base="/">
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
